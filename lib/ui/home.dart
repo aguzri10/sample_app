1a530ee5816c24d81402570d373bb33d42a973d8
AguzR <aguzri10@gmail.com>
2020-01-23 22:27:19 +0700

    third commit

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -11,92 +11,106 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List dataFromJson;
   var loading = false;
   final GlobalKey<RefreshIndicatorState> _refresh =
       GlobalKey<RefreshIndicatorState>();
 
-  Future<void> getData() async {
+  Future<List> getData() async {
     // dataFromJson.clear();
     setState(() {
       loading = true;
     });
 
-    http.Response response = await http.get(Uri.encodeFull(BaseUrl.user_list),
+    http.Response response = await http.get(Uri.encodeFull(BaseUrl.userlist),
         headers: {"Accept": "application/json"});
+
+    // return jsonDecode(response.body);
 
     if (response.statusCode == 200) {
       setState(() {
-        dataFromJson = jsonDecode(response.body);
         loading = false;
       });
+      return jsonDecode(response.body);
     } else {
       print("Something Wrong");
     }
   }
 
   @override
-  void initState() {
-    getData();
-    super.initState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.favorite, color: Colors.white),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.search, color: Colors.white),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.more_vert, color: Colors.white),
+          )
+        ],
+        backgroundColor: Colors.redAccent,
+        leading: Icon(Icons.dehaze, color: Colors.white),
+        title: Text("User List"),
+      ),
+      body: RefreshIndicator(
+        onRefresh: getData,
+        key: _refresh,
+        child: Center(
+          child: loading
+              ? CircularProgressIndicator()
+              : FutureBuilder<List>(
+                  future: getData(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) print(snapshot.error);
+                    return snapshot.hasData
+                        ? ItemList(
+                            list: snapshot.data,
+                          )
+                        : Center(
+                            child: CircularProgressIndicator(),
+                          );
+                  },
+                ),
+        ),
+      ),
+    );
   }
+}
+
+class ItemList extends StatelessWidget {
+  final List list;
+
+  ItemList({this.list});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          actions: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(Icons.favorite, color: Colors.white),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(Icons.search, color: Colors.white),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(Icons.more_vert, color: Colors.white),
-            )
-          ],
-          backgroundColor: Colors.redAccent,
-          leading: Icon(Icons.dehaze, color: Colors.white),
-          title: Text("User List"),
-        ),
-        body: RefreshIndicator(
-          onRefresh: getData,
-          key: _refresh,
-          child: Center(
-            child: loading
-                ? CircularProgressIndicator()
-                : ListView.builder(
-                    itemCount: dataFromJson == null ? 0 : dataFromJson.length,
-                    itemBuilder: (context, i) {
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => UserDetail()));
-                        },
-                        child: Card(
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Padding(padding: EdgeInsets.all(8)),
-                              Icon(Icons.account_circle),
-                              Column(
-                                children: <Widget>[
-                                  Text(dataFromJson[i]['name']),
-                                  Text(dataFromJson[i]['email'])
-                                ],
-                              ),
-                              Icon(Icons.favorite)
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-          ),
-        ));
+    return ListView.builder(
+      itemCount: list == null ? 0 : list.length,
+      itemBuilder: (context, i) {
+        return GestureDetector(
+          onTap: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => UserDetail(
+                  list: list,
+                  index: i
+                )));
+          },
+          child: Card(
+              color: Colors.white,
+              child: ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text(list[i]['name']),
+                subtitle: Text(list[i]['email']),
+                trailing: Icon(Icons.favorite),
+              )),
+        );
+      },
+    );
   }
 }
--- lib/ui/user_detail.dart
+++ lib/ui/user_detail.dart
@@ -1,15 +1,84 @@
 import 'package:flutter/material.dart';
 
 class UserDetail extends StatefulWidget {
+  final List list;
+  final int index;
+
+  UserDetail({this.list, this.index});
+
   @override
   _UserDetailState createState() => _UserDetailState();
 }
 
 class _UserDetailState extends State<UserDetail> {
+
+  final GlobalKey<RefreshIndicatorState> _refresh =
+      GlobalKey<RefreshIndicatorState>();
+  String address, company;
+  var loading;
+  int id;
+
+  setup() {
+    id = widget.list[widget.index]['id'];
+    
+    String street = widget.list[widget.index]['address']['street'];
+    String suite = widget.list[widget.index]['address']['suite'];
+    String city = widget.list[widget.index]['address']['city'];
+    String zipcode = widget.list[widget.index]['address']['zipcode'];
+    String name = widget.list[widget.index]['company']['name'];
+    String catchPhrase = widget.list[widget.index]['company']['catchPhrase'];
+
+    address = street + ', ' + suite + ', ' + city + ', ' + zipcode;
+    company = name + ', ' + catchPhrase;
+
+  }
+
+  @override
+  void initState() {
+    setup();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text(widget.list[widget.index]['name']),
+        leading: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Icon(Icons.arrow_back, color: Colors.white),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Email'),
+            Text(widget.list[widget.index]['email']),
+            SizedBox(height: 16),
+
+            Text('Phone'),
+            Text(widget.list[widget.index]['phone']),
+            SizedBox(height: 16),
+
+            Text('Website'),
+            Text(widget.list[widget.index]['website']),
+            SizedBox(height: 16),
+
+            Text('Address'),
+            Text(address),
+            SizedBox(height: 16),
+
+            Text('Company'),
+            Text(company),
+            SizedBox(height: 16),
+          ],
+        ),
+      )
     );
   }
 }
